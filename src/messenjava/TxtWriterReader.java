6a9933bee9093f9307ef466d10b6132e68cd8c59
Odin94 <kammerloher.andreas@gmail.com>
2016-03-10 00:17:14 +0100

    TxtWriter probably kinda works now

--- src/messenjava/TxtWriterReader.java
+++ src/messenjava/TxtWriterReader.java
@@ -1,9 +1,50 @@
 package messenjava;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author Odin
  */
 public class TxtWriterReader {
+
+    public static void write(String filename, String text) {
+        PrintWriter writer = null;
+
+        try {
+            writer = new PrintWriter(filename, "UTF-8");
+        } catch (Exception f) {
+            f.printStackTrace();
+        }
+        writer.println(text);
+        writer.close();
+    }
+
+    public static String read(String filename) {
+        String content = "";
+        try {
+            content = new Scanner(new File("filename")).useDelimiter("\\Z").next();
+        } catch (Exception f) {
+            f.printStackTrace();
+        }
+        
+        return content;
+    }
+    
+    public static void testMe() {
+        String testText = "this is my text";
+        
+        write("testfile.txt", testText);
+        String readText = read("testfile.txt");
+        
+        if(readText.equals(testText)) {
+            System.out.println("Txtwriterreader seems to work!");
+        }
+        else {
+            System.out.println(testText + " not equal to: " + readText);
+        }
+    }
 
 }
