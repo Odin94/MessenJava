f8248d300f546fa031fef5f2870a9faa4c587f5d
Linda <Linda@Toffeeschachtel.fritz.box>
2016-03-10 13:41:08 +0100

    hmpf

--- src/messenjava/MessenJava.java
+++ src/messenjava/MessenJava.java
@@ -5,18 +5,25 @@
  */
 package messenjava;
 
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  *
@@ -27,25 +34,46 @@ public class MessenJava extends Application {
     int windowWidth = 600;
     int windowheight = 400;
     Label ChatMessages = new Label("start Text");
-    HBox onePerson = new HBox();
-    Label name = new Label();
-    Label onOff = new Label();
+    Label chatingWith=new Label();
+    ObservableList<Peer> data;
 
     @Override
     public void start(Stage primaryStage) {
+        ListView<Peer> list = new ListView<>();
+        data = FXCollections.observableList(new ArrayList());
+        list.setItems(data);
+        
+        
+        list.setCellFactory(new Callback<ListView<Peer>,ListCell<Peer>>(){
+            
+            @Override
+            public ListCell<Peer> call(ListView<Peer> param) {
+                return new ListElement();
+            }
+        });
+        
+        list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                String name=list.getSelectionModel().getSelectedItem().getName();
+                System.out.println("clicked on " + name);
+                chatingWith.setText("Chating with "+name);
+            }
+        });
+        
         HBox BackgroundSeperation = new HBox();
 
         VBox PeopleSection = new VBox();
         PeopleSection.setStyle("-fx-background-color: #336699;");
         PeopleSection.setPadding(new Insets(10, 10, 10, 10));
 
-        onePerson.getChildren().addAll(name, onOff);
-
         Label Header_PeopleSection = new Label("People");
         Header_PeopleSection.setFont(new Font("Arial", 20));
 
-        PeopleSection.getChildren().addAll(Header_PeopleSection, onePerson);
-
+        PeopleSection.getChildren().add(Header_PeopleSection);
+        PeopleSection.getChildren().add(list);
+        
         Label messageLabel = new Label("Message:");
         TextField messageField = new TextField();
 
@@ -68,7 +96,7 @@ public class MessenJava extends Application {
         ChatInputSection.getChildren().addAll(messageLabel, messageField, sendButton);
         ChatInputSection.setSpacing(10);
 
-        oneChat.getChildren().addAll(ChatMessages, ChatInputSection);
+        oneChat.getChildren().addAll(chatingWith,ChatMessages, ChatInputSection);
 
         BackgroundSeperation.getChildren().addAll(PeopleSection, oneChat);
 
@@ -78,7 +106,7 @@ public class MessenJava extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        updateStatus(null, null);
+        updateStatus();
     }
 
     /**
@@ -91,8 +119,7 @@ public class MessenJava extends Application {
         String message;
         //Get message from inputFiel
         message = "hallo von Os";
-        /*Log*/
-        System.out.println(message);
+        /*Log*/System.out.println(message);
 
         //Display message
         display(message, true);
@@ -105,17 +132,16 @@ public class MessenJava extends Application {
         ChatMessages.setText(message);
     }
 
-    private void updateStatus(String name, String online) {
-        name = "heinz ";
-        online = ("on");
-        this.name.setText(name);
-        this.onOff.setText(online);
-
+    private void updateStatus() {
+        data.clear();
+        //dummi Liste
+        data.add(new Peer("anton","111",3,"pubKey","privKey"));
+        data.add(new Peer("peter","111",3,"pubKey","privKey"));
     }
 
     //Todo: attach to onClose event
     private void disconect() {
-
+        System.out.println("disconect");
     }
 
     /**
@@ -131,4 +157,24 @@ public class MessenJava extends Application {
         launch(args);
     }
 
+    class ListElement extends ListCell<Peer>{
+        @Override
+        protected void updateItem(Peer peer, boolean empty){
+            super.updateItem(peer, empty);
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                setText(null);
+
+                HBox seperation=new HBox();
+                Label name=new Label(peer.getName());
+                Label onOff=new Label("off");
+                onOff.setStyle("-fx-background-color: #ff1111;");
+                seperation.getChildren().addAll(name,onOff);
+
+                setGraphic(seperation);
+            }
+        }
+    }
 }
